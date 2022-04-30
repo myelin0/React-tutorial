e4a8ba803953b61fbe31b1d06e76f0f284c510d4
myelin0 <emi.veisllari@gmail.com>
2022-05-01 00:55:24 +0200

    Saving in localstorage using React lifecycle methods

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -16,6 +16,10 @@ class TodoItem extends React.Component{
       this.setState({editing:false})
     }
   }
+
+  componentWillUnmount() {
+  console.log("Cleaning up...")
+}
   render(){
   const completedStyle = {
   fontStyle: "italic",
